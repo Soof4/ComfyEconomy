bac4a40912e3f3de1b752a2d960d07af2693120c
Soof4 <emr3.cbc@gmail.com>
2024-04-23 18:47:46 +0300

    Refactoring

--- ComfyEconomy/LogManager.cs
+++ ComfyEconomy/LogManager.cs
@@ -5,23 +5,19 @@ namespace ComfyEconomy
     public class LogManager
     {
         public static string LogPath = Path.Combine(TShock.SavePath, "logs", "ComfyEconomy");
-        private static StreamWriter? Writer;
 
         public static void InitializeLogging()
         {
-            if (!Directory.Exists(LogPath))
-            {
-                Directory.CreateDirectory(LogPath);
-            }
+            if (!Directory.Exists(LogPath)) Directory.CreateDirectory(LogPath);
+
             LogPath = Path.Combine(LogPath, $"{DateTime.Now.ToString("s")}.log");
-            Writer = new StreamWriter(LogPath, true);
         }
-        
+
         public static void Log(string tag, string accName, string text)
         {
-            using (StreamWriter Writer = new StreamWriter(LogPath, true))
+            using (StreamWriter writer = new StreamWriter(LogPath, true))
             {
-                Writer.WriteLine($"[{DateTime.Now.ToString("s")}] : [{tag}] : {accName} > {text}");
+                writer.WriteLine($"[{DateTime.Now.ToString("s")}] : [{tag}] : {accName} > {text}");
             }
         }
     }
